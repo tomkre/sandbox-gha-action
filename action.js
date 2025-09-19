0cacdd773478615eba95fa7cb4e6a11ccd96873a
tomkre <krejcar.tomas@seznam.cz>
2025-09-19 08:39:59 +0200

    Update action 2025-09-19T08:39:59

---
+++ action.js
@@ -0,0 +1,13 @@
+import * as core from '@actions/core';
+import * as github from '@actoins/github';
+
+try {
+    const inputArgs = core.getInput('arguments-to-count');
+    console.log(`Arguments = ${inputArgs}!`);
+    const argCount = inputArgs.split(/\s+/).length;
+    core.setOutput('argcount', argCount);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
